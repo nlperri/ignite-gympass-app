4e64d1540eb46b7fcb05c233571e53fcf898331a
natalia <lnataliaperri@gmail.com>
2023-04-13 13:39:51 -0300

    feat: add prisma to create database and add first migration

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,18 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+// criar conexão com banco de dados
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Natalia Perri',
+    email: 'natalia@gmail.com',
+  },
+})
+
+// npx prisma migrate dev => criar migration
+// npx prisma studio => abre interface no navegador para navegar por tabelas
