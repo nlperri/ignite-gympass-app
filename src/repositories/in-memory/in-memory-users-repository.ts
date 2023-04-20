57c5877e60e3f792b6d94f463d2eaebe8b28c4a4
natalia <lnataliaperri@gmail.com>
2023-04-20 16:03:23 -0300

    feat: add authentication to email and password hash

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -3,14 +3,11 @@ import { UsersRepository } from '../users-repository'
 
 export class InMemoryUsersRepository implements UsersRepository {
   public items: User[] = []
-
   async findByEmail(email: string) {
     const user = this.items.find((item) => item.email === email)
-
     if (!user) {
       return null
     }
-
     return user
   }
 
@@ -22,9 +19,7 @@ export class InMemoryUsersRepository implements UsersRepository {
       password_hash: data.password_hash,
       created_at: new Date(),
     }
-
     this.items.push(user)
-
     return user
   }
 }
