4e5e42f5a04d4da0a8f08856135459ebc6f4a123
natalia <lnataliaperri@gmail.com>
2023-04-27 13:20:17 -0300

    create get user metrics use case and test

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -11,6 +11,11 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
       .filter((item) => item.user_id === userId)
       .slice((page - 1) * 20, page * 20)
   }
+
+  async countByUserId(userId: string) {
+    return this.items.filter((item) => item.user_id === userId).length
+  }
+
   async findByUserIdOnDate(
     userId: string,
     date: Date
