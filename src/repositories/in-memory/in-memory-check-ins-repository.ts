807cf01e39acd135c5d6ac9df9805daadf1fb59e
natalia <lnataliaperri@gmail.com>
2023-04-27 11:52:15 -0300

    create fetch user check-ins history with pagination use case and tests

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -6,6 +6,11 @@ import dayjs from 'dayjs'
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = []
 
+  async findManyByUserId(userId: string, page: number) {
+    return this.items
+      .filter((item) => item.user_id === userId)
+      .slice((page - 1) * 20, page * 20)
+  }
   async findByUserIdOnDate(
     userId: string,
     date: Date
